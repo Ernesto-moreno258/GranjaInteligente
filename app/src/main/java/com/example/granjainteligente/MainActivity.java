906dd722653088d02d46f5b1cf3b12ee5bec4cee
Ernesto-moreno258 <ernesto.em550@gmail.com>
2020-04-15 00:48:47 +0200

    App

---
+++ app/src/main/java/com/example/granjainteligente/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.granjainteligente;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
